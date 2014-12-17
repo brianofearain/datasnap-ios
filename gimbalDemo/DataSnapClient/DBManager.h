8e95171e29b8e94001a5076bd1cb431d7a561b68
brianofearain <brianjferan@gmail.com>
2014-12-17 11:34:25 -0800

    background upload added

---
+++ gimbalDemo/DataSnapClient/DBManager.h
@@ -0,0 +1,27 @@
+//
+//  DBManager.h
+//  SQLite3DBSample
+//
+//  Created by Gabriel Theodoropoulos on 25/6/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@property (nonatomic, strong) NSMutableArray *arrColumnNames;
+
+@property (nonatomic) int affectedRows;
+
+@property (nonatomic) long long lastInsertedRowID;
+
+
+
+-(instancetype)initWithDatabaseFilename:(NSString *)dbFilename;
+
+-(NSArray *)loadDataFromDB:(NSString *)query;
+
+-(void)executeQuery:(NSString *)query;
+
+@end
